bbece0913fd87496582dc979054ed7fc00c9411d
ahmdhfzh69 <ahmdhfzh69@users.noreply.github.com>
2023-07-31 22:46:55 +0700

    test
    test

---
+++ src/pages/LandingPages.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import Header from 'parts/header'
+export default class LandingPages extends Component {
+    render() {
+      return (
+        <>
+          <Header {...this.props}></Header>
+        </>
+      );
+    }
+  }
